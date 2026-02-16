7029bfdebe53124d50e7d473d9d87e1290c11530
Simon van Stiphout <simon.sundermann@gmail.com>
2026-02-16 17:37:28 +0100

    changing container name

--- workflows/terraform/main.tf
+++ workflows/terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-mgmt-tfstate"
-    storage_account_name = "mgmttfstate"
+    storage_account_name = "kwreduaz01mgmttfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
