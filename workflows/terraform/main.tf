ffd3ce411d06fa298b730007052242c8ab60e93b
Simon van Stiphout <simon.sundermann@gmail.com>
2026-02-17 21:36:40 +0100

    tunneling added

--- workflows/terraform/main.tf
+++ workflows/terraform/main.tf
@@ -196,8 +196,7 @@ resource "azurerm_bastion_host" "bastion" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
-
-  tunneling_enabled = true
+  tunneling_enabled   = true
   
   ip_configuration {
     name                 = "bastion-ipconfig"
